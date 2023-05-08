6066257d889dd3560d50d322788e40831421f92b
Cute King <112088388+CuteKing09@users.noreply.github.com>
2023-05-08 15:11:34 +0530

    Update thumbnails.py

--- AlexaMusic/utils/thumbnails.py
+++ AlexaMusic/utils/thumbnails.py
@@ -92,7 +92,7 @@ async def gen_thumb(videoid, user_id, theme):
         x = f.resize((107, 107))
 
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        bg = Image.open(f"Backgrounds/{theme}.PNG")
+        bg = Image.open(f"Backgrounds/{theme}.png")
         image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
         background = image2.filter(filter=ImageFilter.BoxBlur(30))
