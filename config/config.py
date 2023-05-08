ba420d052e00a1641594653a398da6690b3d1349
Cute King <112088388+CuteKing09@users.noreply.github.com>
2023-05-08 16:30:30 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -38,7 +38,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Cuteboy09/Testing",
+    "https://github.com/Cuteking09/Testing",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
